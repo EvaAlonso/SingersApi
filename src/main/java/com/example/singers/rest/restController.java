982cf221a3f8d15a5e25eb11c40c95dab20ef733
nawud <duwan17@gmail.com>
2025-03-12 14:18:08 +0100

    add delete

--- src/main/java/com/example/singers/rest/restController.java
+++ src/main/java/com/example/singers/rest/restController.java
@@ -1,4 +1,18 @@
 package com.example.singers.rest;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class restController {
+
+
+
+
+
+
+    @DeleteMapping("/{singerId}")
+    public String deleteSinger(@PathVariable int singerId){
+        this.SingerList.removeIf(s -> s.getId() == singerId);
+        return "Acabo la funcion: "+ singerId;
+    }
 }
