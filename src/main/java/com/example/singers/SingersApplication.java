a64a7ff9b394a4d43950fe75919103811daa9f7f
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-03-12 12:40:43 +0100

    initial commit

---
+++ src/main/java/com/example/singers/SingersApplication.java
@@ -0,0 +1,13 @@
+package com.example.singers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingersApplication.class, args);
+	}
+
+}
