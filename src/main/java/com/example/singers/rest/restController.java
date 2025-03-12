8611a3993b5aa58ec41e4bfb9e5bec85d7c81699
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-03-12 14:16:36 +0100

    add loadData

--- src/main/java/com/example/singers/rest/restController.java
+++ src/main/java/com/example/singers/rest/restController.java
@@ -1,4 +1,27 @@
 package com.example.singers.rest;
 
+import com.example.singers.entity.Singer;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
 public class restController {
+
+    List<Singer> singerList;
+
+    @PostConstruct
+    public void loadData(){
+        singerList = new ArrayList<>();
+
+        singerList.add(new Singer(1, "Metallica", "", "Heavy"));
+        singerList.add(new Singer(2, "Chayanne", "", "Pop"));
+        singerList.add(new Singer(3, "Snoop Dogg", "", "Hip Hop"));
+        singerList.add(new Singer(4, "Jerry ", "Rivera", "Salsa"));
+    }
+
 }
